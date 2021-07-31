ff733a501a630f7f7d18b3c158619a3f1732e750
collinskoech11 <collinskoechck34@gmail.com>
2021-07-31 11:09:21 +0300

    added home component

---
+++ ReactNativeApp/screens/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+function Home() {
+    return (
+        <Text>
+            Hello from home 
+        </Text>
+    )
+}
+
+export default Home
