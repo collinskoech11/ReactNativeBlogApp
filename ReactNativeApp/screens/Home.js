ab791d88233eed7627615717c34e358139c39890
collinskoech11 <collinskoechck34@gmail.com>
2021-07-31 15:04:53 +0300

    added data

--- ReactNativeApp/screens/Home.js
+++ ReactNativeApp/screens/Home.js
@@ -3,14 +3,22 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 import {Card} from 'react-native-paper'
 //props is used to send data from one component to another 
 
+const mydata = [
+    {id:1, title:"first Title", description:"first Description"},
+    {id:2, title:"second Title", description:"second Description"},
+    {id:3, title:"third Title", description:"third Description"},
+]
+
 function Home() {
     const [name, setName] = useState("collinskoech11")
     return (
-        <Card style={styles.cardStyle}>        
-            <Text style = {{fontSize:25}}>
-                {name}
-            </Text>
-            <Button title="Change Text" onPress = {() => setName("This is Changed")}/>
+        <Card style={styles.cardStyle}>  
+            <View>     
+                <Text style = {{fontSize:25}}>
+                    {name}
+                </Text>
+                <Button title="Change Text" onPress = {() => setName("This is Changed")}/>
+            </View> 
         </Card>
 
     )
