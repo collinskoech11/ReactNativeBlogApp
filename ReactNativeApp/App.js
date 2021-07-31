675306560ce46c32e54da9e35d92ab5e80f1bea0
collinskoech11 <collinskoechck34@gmail.com>
2021-07-31 12:00:11 +0300

    update home design

--- ReactNativeApp/App.js
+++ ReactNativeApp/App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './screens/Home';
+import {Constants} from 'expo-constants';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.textStyle}>Django and React!</Text>
-      <Text>Hello World</Text>
-     {/** <StatusBar style="auto" /> */}
+      <Home/>
+      {/** <StatusBar style="auto" /> */}
     </View>
   );
 }
@@ -17,9 +18,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#eddfdf',
+    marginTop:20
   },
   textStyle :{
     fontSize:25,
--- ReactNativeApp/screens/Home.js
+++ ReactNativeApp/screens/Home.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native';
+import {Card} from 'react-native-paper'
 
 function Home() {
     return (
-        <Text>
-            Hello from home 
-        </Text>
+        <Card>        
+            <Text>
+                Hello from home 
+            </Text>
+        </Card>
+
     )
 }
-
+const styles = StyleSheet.create({
+    cardStyle: {
+        padding:10,
+        margin:10,
+    }
+})
 export default Home
