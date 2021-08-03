2c767bc3ab30ae902ddda72beb8fd404577112b5
collinskoech11 <collinskoechck34@gmail.com>
2021-08-03 10:09:27 +0300

    useEffect

--- ReactNativeApp/screens/Home.js
+++ ReactNativeApp/screens/Home.js
@@ -1,36 +1,28 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import { StyleSheet, Text, View, FlatList} from 'react-native';
 import {Card, FAB} from 'react-native-paper'
 //props is used to send data from one component to another 
 
-const mydata = [
-    {id:1, title:"first Title", description:"first Description"},
-    {id:2, title:"second Title", description:"second Description"},
-    {id:3, title:"third Title", description:"third Description"},
-    {id:4, title:"first Title", description:"first Description"},
-    {id:5, title:"second Title", description:"second Description"},
-    {id:6, title:"third Title", description:"third Description"},
-    {id:7, title:"first Title", description:"first Description"},
-    {id:8, title:"second Title", description:"second Description"},
-    {id:9, title:"third Title", description:"third Description"},
-]
-const renderData = (item) => {
-    return(
-        <Card style={styles.cardStyle}> 
-        <Text style = {{fontSize:25}}>{item.title}</Text>
-        <Text style = {{fontSize:20}}>{item.description}</Text>
-        </Card> 
-    )
-}
+
 function Home() {
+    const [data, setData] = useState([{title:"First title", description:"first description"}])
+    
+    const renderData = (item) => {
+        return(
+            <Card style={styles.cardStyle}> 
+            <Text style = {{fontSize:25}}>{item.title}</Text>
+            <Text style = {{fontSize:20}}>{item.description}</Text>
+            </Card> 
+        )
+    }
     return (
         <View>
             <FlatList
-                data = {mydata}
+                data = {data}
                 renderItem = {({item}) => {
                     return renderData(item)
                 }}
-                keyExtractor = {item => item.id}
+                keyExtractor = {item => `${item.id}`}
             />
             <FAB
                 style = {styles.fab}
@@ -49,7 +41,7 @@ const styles = StyleSheet.create({
         margin:10,
     },
     fab: {
-        position:'absolute',
+        position:'fixed',
         margin:16,
         right:0,
         bottom:0
