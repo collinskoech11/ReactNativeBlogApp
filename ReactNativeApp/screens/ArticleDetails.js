ef7b7457be7605d38adfa499e59817c128adc75c
collinskoech11 <collinskoechck34@gmail.com>
2021-08-05 12:54:42 +0300

    article details get data

--- ReactNativeApp/screens/ArticleDetails.js
+++ ReactNativeApp/screens/ArticleDetails.js
@@ -10,12 +10,19 @@ function ArticleDetails(props) {
          <Text>{id}</Text>
          <Text style = {{fontSize:25}}>{title}</Text>
          <Text style = {{fontSize:20, marginTop:10}}>{description}</Text>
-         <View>
+         <View style = {styles.btnStyle}>
              <Button
+                style = {styles.buttonStyle}
                 icon = "update"
                 mode = "contained"
                 onPress = {() => console.log("pressed")}
              >Edit</Button>
+             <Button
+                style = {styles.buttonStyle}
+                icon = "delete"
+                mode = "contained"
+                onPress = {() => console.log("pressed")}
+             >Delete</Button>
          </View>
         </View>
     )
@@ -25,6 +32,13 @@ const styles = StyleSheet.create({
     viewStyle: {
         padding:10,
         margin:10
+    },
+    buttonStyle: {
+        marginTop:20
+    },
+    btnStyle: {
+        flexDirection:"row",
+        justifyContent:"space-around",
     }
 })
 export default ArticleDetails
